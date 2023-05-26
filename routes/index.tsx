73cfd380ec65d22c639a6e667bd64bc563afd7df
angel <43898519+AngeelVallem@users.noreply.github.com>
2023-05-25 22:42:23 -0600

    init

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,20 +5,14 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>angel</title>
       </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the
-          ./routes/index.tsx file, and refresh.
-        </p>
-        <Counter start={3} />
-      </div>
+
+      <main class='w-screen h-screen cotainer bg-cyan-500'>
+        <div class='w-full text-center'>
+          <span>angel</span>
+        </div>
+      </main>
     </>
   );
 }
